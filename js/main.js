89d92323ed32f655e059514062824919e65ceecb
raulshma <raulshma@gmail.com>
2018-03-29 00:31:12 +0530

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,31 @@
+var $animation_elements = $('.animation-element');
+var $window = $(window);
+$window.on('scroll resize', check_if_in_view);
+$window.trigger('scroll');
+function check_if_in_view() {
+    var window_height = $window.height();
+    var window_top_position = $window.scrollTop();
+    var window_bottom_position = (window_top_position + window_height);
+  
+    $.each($animation_elements, function() {
+      var $element = $(this);
+      var element_height = $element.outerHeight();
+      var element_top_position = $element.offset().top;
+      var element_bottom_position = (element_top_position + element_height);
+  
+      //check to see if this current container is within viewport
+      if ((element_bottom_position >= window_top_position) &&
+          (element_top_position <= window_bottom_position)) {
+        $element.addClass('in-view');
+      } else {
+        $element.removeClass('in-view');
+      }
+    });
+  }
+
+$(document).ready(function() {
+  $('.curtain-1').addClass('slide-top');
+  $('.curtain-3').addClass('slide-top');
+  $('.curtain-2').addClass('slide-bottom');
+  $('.curtain-4').addClass('slide-bottom');
+});
